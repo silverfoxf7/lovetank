64443abb1dc3ea7f79687c2a5580ac5eecda63ea
Rodrigo Fuentes <joserodrigofuentes@gmail.com>
2011-06-12 10:28:42 -0400

    Fixed ratings weirdness

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -3,19 +3,18 @@ class RatingsController < ApplicationController
 
   def rate
     @asset = Loveaction.find(params[:lid])
-    
     Rating.delete_all(["rateable_type = 'Loveaction' AND rateable_id = ? AND user_id = ?", @asset.id, current_user.id])
     @asset.rate_it(params[:rating], current_user.id)
-
+    #logger.info "Do you see me?"
+    #raise "Do You See Me?"
     @lovely = Loveaction.find(params[:lid])
-    @temp = Relationship.find_by_follower_id(params[:id])
-    @lovely.recip_id = User.find(@temp.followed_id)
-    @lovely.recip_rating = params[:rating]
-    @lovely.save
-
-    render :update do |page|
-      page.replace_html 'star-ratings-block', :partial => 'ratings/rating', :locals => { :asset => @asset }
-      page.visual_effect :highlight, 'rate'
-    end
+    @lovely.recip_rating = @asset.average_rating
+#    @lovely.save
+    @lovely.update_attribute(:recip_rating, @asset.average_rating)
+    #render :update do |page|
+    #  page.replace_html 'star-ratings-block', :partial => 'ratings/rating', :locals => { :asset => @asset }
+    #  page.visual_effect :highlight, 'rate'
+      redirect_to(@asset.user)
+    #end
   end
 end
