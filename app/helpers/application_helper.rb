4540fbacdd2ed1e4ac1716a33f7ecb9d4f32d0ab
Rodrigo Fuentes <joserodrigofuentes@gmail.com>
2011-06-16 05:54:53 -0400

    fixing heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Return a title on a per-page basis.
   def title
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Show Me the Love"
     if @title.nil?
       base_title
     else
