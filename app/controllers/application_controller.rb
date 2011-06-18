adbfa38e165dd5bed2d5ac5b066776a0f855411a
Amit Jotwani <Amit@Woody.local>
2011-06-17 23:39:37 -0400

    my first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+  
+  before_filter :app_status
+end
+
+def app_status
+  @app_status = "alpha"
 end
